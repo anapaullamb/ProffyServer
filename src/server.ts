4757b86511c28dc4bb6d1d82fad49db6ec812605
Ana Paula <apaula@localhost.localdomain>
2020-08-30 18:30:32 -0400

    API finalizada

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import express from "express";
+import routes from "./routes";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
-app.get("/", (request, response) => {
-  return response.json({ message: "Helo World" });
-});
+app.use(routes);
 app.listen(3333);
